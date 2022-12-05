2da1f7b20af8709e673137281ec840d580074f31
keyser6 <118495830+keyser6@users.noreply.github.com>
2022-12-04 21:15:15 -0300

    Update Calculo-ticket.js

--- Calculo-ticket.js
+++ Calculo-ticket.js
@@ -3,7 +3,7 @@ const precioticket = 200 ;
 function calculo() {
     var cantidadtickets = document.getElementById ('inputCantidad').value ;
     var desc= document.getElementById('inputCategoria').value ;
-    var preciounidadcondesc= (precioticket * desc) / 100;
+    var preciounidadcondesc= (precioticket * desc);
     var precio= cantidadtickets * preciounidadcondesc;
     document.getElementById ('comprafinal').value= 'Total a pagar: $'+ precio;
 }
